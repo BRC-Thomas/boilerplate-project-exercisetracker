309628a26ba536f53bf71491592ca34986836e91
BRC-Thomas <thomasbaracani@gmail.com>
2024-07-31 10:01:27 +0200

    models

---
+++ models/Exercise.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const ExerciseSchema = new mongoose.Schema({
+  user_id: {type: String, required: true},
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+module.exports = mongoose.model("user", ExerciseSchema);
